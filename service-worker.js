87f77c9499cb396783d3ac37f15e8df754868cc7
Sapao <80600770+matheus-sousaa@users.noreply.github.com>
2022-03-26 00:56:56 +0000

    config adjusted and cache treated

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,37 @@
 const cacheName = 'water_me-pwa-v1.0';
+const htmlFiles = ['/index.html', '/config.html', '/dia-semana.html', '/hoje.html', '/ontem.html'];
 
-// Armazena todos os arquivos no cache atual
-/*self.addEventListener('install', function (event) {
-  caches.open(cacheName).then((cache) => {
-    cache.addAll([]);
-  });
-});*/
+const cssFiles = [
+  '/styles/global.css',
+  '/styles/config.css',
+];
+
+const jsFiles = [
+  '/javascript/global.js',
+  '/javascript/config.js',
+];
+
+const assetsFiles = [
+  'assets/botao-voltar.svg',
+  'assets/logo-192px.png',
+  'assets/logo-512px.png',
+  'assets/icon.png',
+  'assets/logo-header.png',
+  'assets/seta-direita.svg',
+];
+
+const pathsToCache = [
+  '/',
+  '/manifest.webmanifest',
+  ...htmlFiles,
+  ...cssFiles,
+  ...jsFiles,
+  ...assetsFiles,
+];
+
+self.addEventListener('install', (e) => {
+  caches.open(cacheName).then((cache) => cache.addAll(pathsToCache));
+});
 
 // Recupera todos os nomes de cache e apaga aqueles
 // que forem diferentes do cache atual
@@ -42,7 +68,7 @@ self.addEventListener('activate', (e) => {
 // Tenta servir o arquivo do cache atual. Se não for possível,
 // baixa o recurso da web e o armazena localmente, antes de entregar
 // uma cópia para o usuário.
-self.addEventListener('fetch', function (event) {
+/*self.addEventListener('fetch', function (event) {
   let response = caches.open(cacheName).then((cache) => {
     return cache.match(event.request).then((resource) => {
       if (resource) return resource;
@@ -54,3 +80,4 @@ self.addEventListener('fetch', function (event) {
   });
   event.respondWith(response);
 });
+*/
