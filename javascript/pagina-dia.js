8a7aea879896c8f58ac43c9493918c477428f685
Sapao <80600770+matheus-sousaa@users.noreply.github.com>
2022-03-30 02:31:37 +0000

    removing and adding cups in todays button

--- javascript/pagina-dia.js
+++ javascript/pagina-dia.js
@@ -4,7 +4,7 @@ window.addEventListener('load', () => {
     const config = JSON.parse(configDefined)
     document.getElementById("cupsNumber").innerHTML = `${config.cupsNumber}`
     const fullCupList = document.querySelector("ul#fullCups")
-    for (let i=0; i<config.cupsNumber; i++) {
+    for (let i=0; i < config.cupsNumber; i++) {
       const cupElement = document.createElement("li")
       cupElement.innerHTML = `
       <img src="assets/copo-cheio.svg">
@@ -12,17 +12,48 @@ window.addEventListener('load', () => {
       fullCupList.appendChild(cupElement)
     }
     const span = document.querySelector("div span")
-    const button = document.querySelector("div button#plusButton")
+    const plusButton = document.querySelector("div button#plusButton")
+    const lessButton = document.querySelector("div button#lessButton")
+    const emptyCupList = document.querySelector("ul#emptyCups")
+    span.addEventListener("onchange", console.log)
+    
     window.less = () => {
       span.innerHTML = +span.innerHTML > 0 ? +span.innerHTML - 1 : 0;
-      button.removeAttribute("disabled", true)
+      
+      if (+span.innerHTML === 0) {
+        lessButton.setAttribute("disabled", true)
+      }
+      
+      plusButton.removeAttribute("disabled", true)
+      emptyCupList.removeChild(emptyCupList.firstChild)
+
+      const cupElement = document.createElement("li")
+      cupElement.innerHTML = `
+      <img src="assets/copo-cheio.svg">
+      `
+      fullCupList.appendChild(cupElement)
     }
+    
     window.plus = () => {
       span.innerHTML = +span.innerHTML >= 0 && +span.innerHTML < config.cupsNumber ? +span.innerHTML + 1 : 0;
-
-      if (+span.innerHTML === +config.cupsNumber) {
-      button.setAttribute("disabled", true)
+      lessButton.removeAttribute("disabled", true)
+      
+        if (+span.innerHTML === +config.cupsNumber) {
+        plusButton.setAttribute("disabled", true)
+      }   
+      
+      const cupElement = document.createElement("li")
+      cupElement.innerHTML = `
+      <img src="assets/copo-vazio.svg">
+      `
+      emptyCupList.appendChild(cupElement)
+      fullCupList.removeChild(fullCupList.firstChild)
     }
+
+    function saveOnLocalStorage () {
+      localStorage.setItem("drinkedCups", span.innerHTML)
+      config.cupsNumber += 1
+      localStorage.setItem("config", configDefined)
     }
   }
 })
